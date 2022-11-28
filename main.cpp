da8c3f20c30f54039e83485835a1209a72c33fcd
Stefanomarelli <117829913+5590355@users.noreply.github.com>
2022-11-28 13:08:10 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int a=1, b=1, c=0, n=0;
+    cin>>n;
+    cout<<a<<endl<<b<<endl;
+    for (int i=2; i<=n; i++) {
+        c=a+b;
+        a=b;
+        b=c;
+        if (c<=n) {
+            cout<<c<<endl;
+        }
+    }
+    return 0;
 }
